4b3291f8c76331d68020af96c1353e4557a3e051
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-12-02 22:39:38 -0800

    Added requirements.txt

--- Chess_Game/Main.py
+++ Chess_Game/Main.py
@@ -7,32 +7,30 @@ from array import *
 from Pieces import *
 from BoardState import *
 
-
-
 global currentPlayerColor, currentPieceSelected, currentPossibleMoves
-
-
 
 MAX_DEPTH = 2
 
-#returns the color a tile should be based on its position
-def getTileColor(x,y):
+
+# returns the color a tile should be based on its position
+def getTileColor(x, y):
     if (x % 2 == 0):
-        if(y % 2 == 0):
+        if (y % 2 == 0):
             return 'white'
         else:
             return 'black'
     else:
-        if(y % 2 == 0):
+        if (y % 2 == 0):
             return 'black'
         else:
             return 'white'
 
-#color - color of the player to evaluate in relation to (color of whatever player the AI is)
-#tree - the state tree to evaluate (pass the root node of the current board)
-#minmax - whether the current node is a min or a max (should be max)
+
+# color - color of the player to evaluate in relation to (color of whatever player the AI is)
+# tree - the state tree to evaluate (pass the root node of the current board)
+# minmax - whether the current node is a min or a max (should be max)
 def abMinMax(color, tree, minmax):
-    if(len(tree.children) == 0):
+    if (len(tree.children) == 0):
         tree.eval = tree.state.evaluate(color)
         return tree
     else:
@@ -53,62 +51,61 @@ def abMinMax(color, tree, minmax):
             return tree
 
 
-#create the board and window
+# create the board and window
 board = BoardState()
 window = tk.Tk()
-imageFiles = [tk.PhotoImage(file = 'images/Pawn_Black.png'),
-              tk.PhotoImage(file = 'images/Rook_Black.png'),
-              tk.PhotoImage(file = 'images/Knight_Black.png'),
-              tk.PhotoImage(file = 'images/Bishop_Black.png'),
-              tk.PhotoImage(file = 'images/Queen_Black.png'),
-              tk.PhotoImage(file = 'images/King_Black.png'),
-              tk.PhotoImage(file = 'images/Pawn_White.png'),
-              tk.PhotoImage(file = 'images/Rook_White.png'),
-              tk.PhotoImage(file = 'images/Knight_White.png'),
-              tk.PhotoImage(file = 'images/Bishop_White.png'),
-              tk.PhotoImage(file = 'images/Queen_White.png'),
-              tk.PhotoImage(file = 'images/King_White.png')]
+imageFiles = [tk.PhotoImage(file='images/Pawn_Black.png'),
+              tk.PhotoImage(file='images/Rook_Black.png'),
+              tk.PhotoImage(file='images/Knight_Black.png'),
+              tk.PhotoImage(file='images/Bishop_Black.png'),
+              tk.PhotoImage(file='images/Queen_Black.png'),
+              tk.PhotoImage(file='images/King_Black.png'),
+              tk.PhotoImage(file='images/Pawn_White.png'),
+              tk.PhotoImage(file='images/Rook_White.png'),
+              tk.PhotoImage(file='images/Knight_White.png'),
+              tk.PhotoImage(file='images/Bishop_White.png'),
+              tk.PhotoImage(file='images/Queen_White.png'),
+              tk.PhotoImage(file='images/King_White.png')]
 
-
-#create all the pawns
+# create all the pawns
 for i in range(8):
-    board.addPiece(Pawn(i,1,'black',PAWN_BLACK))
+    board.addPiece(Pawn(i, 1, 'black', PAWN_BLACK))
 
 for i in range(8):
-    board.addPiece(Pawn(i,6,'white',PAWN_WHITE))
-#create all the Rooks
-board.addPiece(Rook(0,0,'black', ROOK_BLACK))
-board.addPiece(Rook(7,0,'black', ROOK_BLACK))
-board.addPiece(Rook(0,7,'white', ROOK_WHITE))
-board.addPiece(Rook(7,7,'white', ROOK_WHITE))
-#Create all the Knights
-board.addPiece(Knight(1,0,'black', KNIGHT_BLACK))
-board.addPiece(Knight(6,0,'black', KNIGHT_BLACK))
-board.addPiece(Knight(1,7,'white', KNIGHT_WHITE))
-board.addPiece(Knight(6,7,'white', KNIGHT_WHITE))
-#Create all the Bishops
-board.addPiece(Bishop(2,0,'black', BISHOP_BLACK))
-board.addPiece(Bishop(5,0,'black', BISHOP_BLACK))
-board.addPiece(Bishop(2,7,'white', BISHOP_WHITE))
-board.addPiece(Bishop(5,7,'white', BISHOP_WHITE))
-#Create the Queens
-board.addPiece(Queen(3,0,'black', QUEEN_BLACK))
-board.addPiece(Queen(3,7,'white', QUEEN_WHITE))
-#Create the Kings
-board.addPiece(King(4,0,'black', KING_BLACK))
-board.addPiece(King(4,7,'white', KING_WHITE))
+    board.addPiece(Pawn(i, 6, 'white', PAWN_WHITE))
+# create all the Rooks
+board.addPiece(Rook(0, 0, 'black', ROOK_BLACK))
+board.addPiece(Rook(7, 0, 'black', ROOK_BLACK))
+board.addPiece(Rook(0, 7, 'white', ROOK_WHITE))
+board.addPiece(Rook(7, 7, 'white', ROOK_WHITE))
+# Create all the Knights
+board.addPiece(Knight(1, 0, 'black', KNIGHT_BLACK))
+board.addPiece(Knight(6, 0, 'black', KNIGHT_BLACK))
+board.addPiece(Knight(1, 7, 'white', KNIGHT_WHITE))
+board.addPiece(Knight(6, 7, 'white', KNIGHT_WHITE))
+# Create all the Bishops
+board.addPiece(Bishop(2, 0, 'black', BISHOP_BLACK))
+board.addPiece(Bishop(5, 0, 'black', BISHOP_BLACK))
+board.addPiece(Bishop(2, 7, 'white', BISHOP_WHITE))
+board.addPiece(Bishop(5, 7, 'white', BISHOP_WHITE))
+# Create the Queens
+board.addPiece(Queen(3, 0, 'black', QUEEN_BLACK))
+board.addPiece(Queen(3, 7, 'white', QUEEN_WHITE))
+# Create the Kings
+board.addPiece(King(4, 0, 'black', KING_BLACK))
+board.addPiece(King(4, 7, 'white', KING_WHITE))
 
 print("Board Evaluation (White):", board.evaluate("white"))
 print("Board Evaluation (Black):", board.evaluate("black"))
 print()
-#tree = board.createTree('white')
-#newBoard = abMinMax('white', tree, 'max')
+# tree = board.createTree('white')
+# newBoard = abMinMax('white', tree, 'max')
 
 canvas = tk.Canvas(window, bg='white', width=800, height=800)
 
-#holds all the polygons so they can be redrawn later
+# holds all the polygons so they can be redrawn later
 matrix = np.zeros((8, 8))
-#holds all the images so they can be redrawn later
+# holds all the images so they can be redrawn later
 images = []
 
 row_index = -1
@@ -117,20 +114,20 @@ for r in range(8):
     col_index = -1
     for c in range(8):
         col_index += 1
-        tx = c*100
-        ty = r*100
-        #set the coordinates of the four corners of the tile 
+        tx = c * 100
+        ty = r * 100
+        # set the coordinates of the four corners of the tile
         coord = tx, ty, tx + 100, ty, tx + 100, ty + 100, tx, ty + 100
-        #creates a polygon to represent a tile on the board
-        box = canvas.create_polygon(coord, fill = getTileColor(r,c))
+        # creates a polygon to represent a tile on the board
+        box = canvas.create_polygon(coord, fill=getTileColor(r, c))
         # Stores every polygon created to make the black and white checker board based on coordinates
         matrix[col_index, row_index] = box
 
 for i in board.blackPieces:
-    image_id = canvas.create_image(i.x * 100, i.y * 100, anchor = tk.NW, image = imageFiles[i.image])
+    image_id = canvas.create_image(i.x * 100, i.y * 100, anchor=tk.NW, image=imageFiles[i.image])
     i.canvas_image = image_id
 for i in board.whitePieces:
-    image_id = canvas.create_image(i.x * 100, i.y * 100, anchor=tk.NW, image = imageFiles[i.image])
+    image_id = canvas.create_image(i.x * 100, i.y * 100, anchor=tk.NW, image=imageFiles[i.image])
     i.canvas_image = image_id
 
 currentPlayerColor = 'white'
@@ -143,7 +140,7 @@ matrix_int = matrix.astype(np.int)  # Convert Matrix from float 64 to int so ite
 def buttonPressed(event):
     global currentPlayerColor, currentPieceSelected, currentPossibleMoves, changed_tiles
     canvas.focus_set()
-    if(event.x < 10 or event.y < 0 or event.y > 800 or event.x > 800):
+    if (event.x < 10 or event.y < 0 or event.y > 800 or event.x > 800):
         return
 
     tileX = int(event.x / 100)
@@ -173,19 +170,18 @@ def buttonPressed(event):
                     piece_to_delete.y = None
 
                 canvas.delete(canvas_image)
-                previous_piece.canvas_image = canvas.create_image(coordX, coordY, anchor= tk.NW, image=image)
+                previous_piece.canvas_image = canvas.create_image(coordX, coordY, anchor=tk.NW, image=image)
 
-                #Update board State
+                # Update board State
                 previous_piece.x = tileX
                 previous_piece.y = tileY
                 previous_piece.moved = True
 
                 # Switch colors
-                if(currentPlayerColor == "white"):
+                if (currentPlayerColor == "white"):
                     currentPlayerColor = "black"
-                elif(currentPlayerColor == "black"):
+                elif (currentPlayerColor == "black"):
                     currentPlayerColor = "white"
-
 
         # Clear changed tiles after
         changed_tiles = []
@@ -213,13 +209,17 @@ def buttonPressed(event):
                     # Store the tile and the color of the coordinate so it
                     # can be reverted back later
                     changed_tiles.append((item, original_color))
-                    canvas.itemconfig(item, fill = "blue", width = "5", outline = "red")
+                    canvas.itemconfig(item, fill="blue", width="5", outline="red")
     currentPieceSelected = current_piece
 
 
 canvas.bind('<Button-1>', buttonPressed)
+
+
 def checkPiece(piece):
     return piece.type
+
+
 canvas.bind("<Button-1>", buttonPressed)
 canvas.pack()
 
